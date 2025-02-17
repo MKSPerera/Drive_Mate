6108f57a2fd9067eb384028aeb4eb788a874cb0a
ThisenDewnim <thisendissanayake3@gmail.com>
2025-02-17 16:48:08 +0530

    created the building function of main.dart

--- notification/lib/main.dart
+++ notification/lib/main.dart
@@ -4,3 +4,19 @@ import 'notification.dart';
 void main() {
   runApp(const MyApp());
 }
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        scaffoldBackgroundColor: const Color(0xFFF0F2F6),
+      ),
+      home: const NotificationsScreen(),
+    );
+  }
+}

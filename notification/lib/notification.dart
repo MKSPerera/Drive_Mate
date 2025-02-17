a306308e03420eb4657505f0269f6c337eaafd3b
ThisenDewnim <thisendissanayake3@gmail.com>
2025-02-17 16:53:34 +0530

    edited the notification screen

--- notification/lib/notification.dart
+++ notification/lib/notification.dart
@@ -75,3 +75,77 @@ class NotificationsScreen extends StatelessWidget {
       ),
     );
   }
+
+  Widget _buildNotificationCard({
+    required String message,
+    required String time,
+    required bool isCancel,
+  }) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: isCancel
+              ? [const Color(0xFFFFE5E5), const Color(0xFFFFECEC)]
+              : [const Color(0xFFE0D6FF), const Color(0xFFF3EFFF)],
+          begin: Alignment.centerLeft,
+          end: Alignment.centerRight,
+        ),
+        borderRadius: BorderRadius.circular(16),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.05),
+            blurRadius: 8,
+            offset: const Offset(0, 2),
+          ),
+        ],
+      ),
+      child: ListTile(
+        contentPadding: const EdgeInsets.all(16),
+        leading: Container(
+          padding: const EdgeInsets.all(10),
+          decoration: BoxDecoration(
+            color: (isCancel ? Colors.red : Colors.green).withOpacity(0.1),
+            shape: BoxShape.circle,
+          ),
+          child: Icon(
+            isCancel ? Icons.cancel : Icons.check_circle,
+            color: isCancel ? Colors.red : Colors.green,
+            size: 28,
+          ),
+        ),
+        title: Text(
+          message,
+          style: const TextStyle(
+            fontSize: 15,
+            color: Color(0xFF2D3142),
+            height: 1.4,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+        subtitle: Padding(
+          padding: const EdgeInsets.only(top: 8),
+          child: Text(
+            time,
+            style: TextStyle(
+              fontSize: 13,
+              color: const Color(0xFF2D3142).withOpacity(0.6),
+            ),
+          ),
+        ),
+        trailing: Container(
+          padding: const EdgeInsets.all(8),
+          decoration: BoxDecoration(
+            color: Colors.white.withOpacity(0.5),
+            shape: BoxShape.circle,
+          ),
+          child: const Icon(
+            Icons.chevron_right,
+            color: Color(0xFF2D3142),
+            size: 20,
+          ),
+        ),
+        onTap: () {},
+      ),
+    );
+  }
+}

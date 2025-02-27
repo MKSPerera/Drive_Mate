5a335a5706d3e5e5b197323d7bc244b6bccb3893
DarrenArmous <the1darren2@gmail.com>
2025-02-27 12:01:08 +0530

    Mage the job management page and added necessary imports

---
+++ frontend-drivemate/app/job-management/page.tsx
@@ -0,0 +1,7 @@
+import { AddJobForm } from "@/components/add-job-form"
+import { JobList } from "@/components/job-list"
+import { Navbar } from "@/components/navbar"
+import { Sidebar } from "@/components/sidebar"
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+import { useState } from "react"

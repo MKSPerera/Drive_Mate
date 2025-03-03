f8aca77c05966535bccb73ef9931901acaa7e7c5
viduranga321 <iruka.20230997@iit.ac.lk>
2025-03-03 21:41:00 +0530

    added create-job.dto.ts

---
+++ backend/src/jobs/dto/create-job.dto.ts
@@ -0,0 +1,52 @@
+import { JobState } from '@prisma/client';
+import { IsDateString, IsEnum, IsInt, IsNotEmpty, IsNumber, IsOptional, IsString } from 'class-validator';
+
+export class CreateJobDto {
+  @IsString()
+  @IsNotEmpty()
+  clientName: string;
+
+  @IsString()
+  @IsNotEmpty()
+  nationality: string;
+
+  @IsInt()
+  @IsNotEmpty()
+  numberOfPassengers: number;
+
+  @IsString()
+  @IsNotEmpty()
+  pickupLocation: string;
+
+  @IsDateString()
+  @IsNotEmpty()
+  startDate: string;
+
+  @IsDateString()
+  @IsNotEmpty()
+  endDate: string;
+
+  @IsDateString()
+  @IsNotEmpty()
+  pickupTime: string;
+
+  @IsNumber()
+  @IsNotEmpty()
+  distance: number;
+
+  @IsNumber()
+  @IsNotEmpty()
+  paymentAmount: number;
+
+  @IsString()
+  @IsOptional()
+  additionalDetails?: string;
+
+  @IsEnum(JobState)
+  @IsOptional()
+  currentState?: JobState;
+
+  @IsInt()
+  @IsOptional()
+  assignedDriverId?: number;
+}

a24c6b6a74da033deef4cb04c1bf909789ef8df6
ThisenDewnim <thisendissanayake3@gmail.com>
2025-03-03 22:42:11 +0530

    created the accepted completed jobs page

---
+++ frontend/lib/profile/accepted_completed_jobs_page.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AcceptedCompletedJobsPage extends StatefulWidget {
+  const AcceptedCompletedJobsPage({super.key});
+
+  @override
+  _AcceptedCompletedJobsPageState createState() => _AcceptedCompletedJobsPageState();
+}

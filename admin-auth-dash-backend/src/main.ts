f0fdf461bc87f569d915c7b9be5254536740d1b9
Chanith Ransika <chanith.20231027@iit.ac.lk>
2025-03-03 18:07:55 +0530

    updated main.ts

--- admin-auth-dash-backend/src/main.ts
+++ admin-auth-dash-backend/src/main.ts
@@ -1,15 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
   
   // Enable CORS
-  app.enableCors();
-  
-  await app.listen(3333);
-  console.log(`Application is running on: ${await app.getUrl()}`);
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+  });
+
+  await app.listen(process.env.PORT ?? 3333);
 }
 bootstrap();

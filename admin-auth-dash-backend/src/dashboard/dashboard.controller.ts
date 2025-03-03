c254bf5772e112d9d056848aba8a2619b5c5c34b
Chanith Ransika <chanith.20231027@iit.ac.lk>
2025-03-03 17:39:06 +0530

    created service, controller and module of dashboard

---
+++ admin-auth-dash-backend/src/dashboard/dashboard.controller.ts
@@ -0,0 +1,9 @@
+import { Controller, Get } from '@nestjs/common';
+import { DashboardService } from './dashboard.service';
+
+@Controller('dashboard')
+export class DashboardController {
+  constructor(private readonly dashboardService: DashboardService) {}
+
+
+} 
---
+++ admin-auth-dash-backend/src/dashboard/dashboard.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { JobState } from '@prisma/client';
+
+@Injectable()
+export class DashboardService {
+ 
+  
+} 

7dfd5df3ca1f93f2e81f7498fdfbc430b43ac518
ThisenDewnim <thisendissanayake3@gmail.com>
2025-03-03 23:24:46 +0530

    created the profilescreen class

---
+++ frontend/lib/profile/profile.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import '../nav_bar/nav_bar.dart';
+import '../services/secure_storage_service.dart';
+import 'personal_information_page.dart';
+import 'vehicle_information_profile.dart';
+import 'accepted_completed_jobs_page.dart';
+import 'status_performance_page.dart';
+import 'legal_terms_page.dart';
+import 'settings_page.dart';
+
+class ProfileScreen extends StatefulWidget {
+  const ProfileScreen({super.key});
+
+  @override
+  _ProfileScreenState createState() => _ProfileScreenState();
+}

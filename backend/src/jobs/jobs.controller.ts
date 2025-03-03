a7b25895f22e30e7bbb00a340c1f37f501e2822e
viduranga321 <iruka.20230997@iit.ac.lk>
2025-03-03 21:37:40 +0530

    added jobs.controller.ts

---
+++ backend/src/jobs/jobs.controller.ts
@@ -0,0 +1,61 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete, HttpCode, HttpStatus, UseGuards, Req } from '@nestjs/common';
+import { JobsService } from './jobs.service';
+import { CreateJobDto } from './dto/create-job.dto';
+import { UpdateJobDto } from './dto/update-job.dto';
+import { AcceptJobDto } from './dto/accept-job.dto';
+import { DriverAuthGuard } from '../driver/guards/driver-auth.guard';
+
+@Controller('jobs')
+export class JobsController {
+  constructor(private readonly jobsService: JobsService) {}
+
+  @Post()
+  create(@Body() createJobDto: CreateJobDto) {
+    return this.jobsService.create(createJobDto);
+  }
+
+  @Post('accept') // Use POST request to accept a job
+  @HttpCode(HttpStatus.OK) // Respond with 200 OK
+  async acceptJob(@Body() acceptJobDto: AcceptJobDto) {
+    try {
+      const updatedJob = await this.jobsService.acceptJob(acceptJobDto);
+      return {
+        status: 'success',
+        message: 'Job accepted successfully',
+        data: updatedJob,
+      };
+    } catch (error) {
+      return {
+        status: 'error',
+        message: error.message,
+      };
+    }
+  }
+
+  @Get()
+  findAll() {
+    return this.jobsService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.jobsService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateJobDto: UpdateJobDto) {
+    return this.jobsService.update(+id, updateJobDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.jobsService.remove(+id);
+  }
+
+  @Get('my-jobs')
+  @UseGuards(DriverAuthGuard)
+  async getMyJobs(@Req() req) {
+    const driverId = req.user.driverId;
+    return this.jobsService.findJobsByDriver(driverId);
+  }
+}

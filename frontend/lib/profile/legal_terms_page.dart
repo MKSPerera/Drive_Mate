bf804f08ad29df21530dd78fd38aba8de4244e86
ThisenDewnim <thisendissanayake3@gmail.com>
2025-03-03 23:06:24 +0530

    Added Legal & Terms screen with AppBar styling

--- frontend/lib/profile/legal_terms_page.dart
+++ frontend/lib/profile/legal_terms_page.dart
@@ -2,3 +2,19 @@ import 'package:flutter/material.dart';
 
 class LegalTermsPage extends StatelessWidget {
   const LegalTermsPage({super.key});
+
+   @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFFEDE1FF),
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        title: const Text(
+          'Legal & Terms',
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 20,
+            fontWeight: FontWeight.w600,
+          ),
+        ),

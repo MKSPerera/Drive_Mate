c2c6389bc25b574042f38e5b8f9207ebd97d0efd
Chanith Ransika <chanith.20231027@iit.ac.lk>
2025-03-23 22:01:01 +0530

    updated the module of admin auth

---
+++ backend/src/admin/admin.module.ts
@@ -0,0 +1,22 @@
+// src/admin/admin.module.ts
+import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { AdminController } from './admin.controller';
+import { AdminService } from './admin.service';
+
+@Module({
+  imports: [
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get('JWT_SECRET'),
+        signOptions: { expiresIn: '1h' },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+  controllers: [AdminController],
+  providers: [AdminService],
+})
+export class AdminModule {}
